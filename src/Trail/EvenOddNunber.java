0de812d17926c997a25cd3d0c1c355d749881c4b
Vinod7875 <vinod.narwade@kemsys.com>
2024-05-28 10:55:00 +0530

    added even number in programm

--- src/Trail/EvenOddNunber.java
+++ src/Trail/EvenOddNunber.java
@@ -1,9 +1,13 @@
 package Trail;
 
 public class EvenOddNunber {
-	
+
 	public static void main(String[] args) {
-		
+		for (int i = 1; i < 100; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
 	}
 
 }
