c652c11b23a7ae75fff0f09e9bf25523dafd3014
Vinod7875 <vinod.narwade@kemsys.com>
2024-05-31 17:53:36 +0530

    chnages

--- src/Trail/SwapNumber.java
+++ src/Trail/SwapNumber.java
@@ -5,12 +5,12 @@ public class SwapNumber {
 	public static void main(String[] args) {
 		int a = 12, b = 25;
 
-		a = a+ b;
+		a = a + b;
 		b = a - b;
 		a = a - b;
 
-		System.out.println("a = " + a);
-		System.out.println("b = " + b);
+		System.out.println("a= " + a);
+		System.out.println("b= " + b);
 	}
 
 }
