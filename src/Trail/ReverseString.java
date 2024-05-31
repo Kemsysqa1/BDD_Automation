e8c4574738f34a61481e64e9b20c794c9523cdbf
Vinod7875 <vinod.narwade@kemsys.com>
2024-05-31 15:25:12 +0530

    reverse string programm added

---
+++ src/Trail/ReverseString.java
@@ -0,0 +1,15 @@
+package Trail;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String str = "Kem";
+		String rev = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			char ch = str.charAt(i);
+			rev += ch;
+		}
+		System.out.println(rev);
+	}
+
+}
