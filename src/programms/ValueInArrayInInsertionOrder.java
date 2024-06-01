8ee97b00f8f740e90aa3ba4ced903a864dd444b3
Vinod7875 <vinod.narwade@kemsys.com>
2024-06-01 20:12:58 +0530

    array simple programm

---
+++ src/programms/ValueInArrayInInsertionOrder.java
@@ -0,0 +1,15 @@
+package programms;
+
+public class ValueInArrayInInsertionOrder {
+
+	public static void main(String[] args) {
+		int[] num = new int[10];
+
+		for (int i = 0; i < 10; i++) {
+			num[i] = i;
+			System.out.println(num[i]);
+		}
+
+	}
+
+}
