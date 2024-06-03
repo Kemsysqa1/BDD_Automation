35e3f44aad20ee2cf9a04ee392bf874f3856358b
Vinod7875 <vinod.narwade@kemsys.com>
2024-06-03 10:01:00 +0530

    Solve this problem

---
+++ src/programms/ValueInArrayInUnOrder.java
@@ -0,0 +1,13 @@
+package programms;
+
+public class ValueInArrayInUnOrder {
+
+	public static void main(String[] args) {
+		int[] num = new int[10];
+		int[] no = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		for (int i = 0; i < 10; i++) {
+			System.out.println(no[i]);	
+		}
+	}
+
+}
