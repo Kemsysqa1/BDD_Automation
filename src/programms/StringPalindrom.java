940e4c4ca52c55b8cfa3b628628899af68c2a411
Vinod7875 <vinod.narwade@kemsys.com>
2024-06-03 11:04:13 +0530

    string palindrome programm

---
+++ src/programms/StringPalindrom.java
@@ -0,0 +1,20 @@
+package programms;
+
+public class StringPalindrom {
+
+	public static void main(String[] args) {
+		String name = "TeT";
+		String temp = name;
+		String revStr = "";
+		for (int i = name.length() - 1; i >= 0; i--) {
+			char ch = name.charAt(i);
+			revStr += ch;
+		}
+		if (temp.equals(revStr)) {
+			System.out.println("Stirng is palindrome: " + revStr);
+		} else {
+			System.out.println("String is not palindrome: " + revStr);
+		}
+	}
+
+}
