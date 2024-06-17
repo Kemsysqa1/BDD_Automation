7f47627f10fb221cf1439dd7b4e29d86f5a0ca7d
Vinod7875 <vinod.narwade@kemsys.com>
2024-06-17 11:29:40 +0530

    changes

--- src/starPatterns/Star3.java
+++ src/starPatterns/Star3.java
@@ -3,12 +3,12 @@ package starPatterns;
 public class Star3 {
 
 	public static void main(String[] args) {
-		int n = 5;
+		int n = 6;
 
 		for (int i = 1; i < n * 2; i++) {
 			int row = i < n ? i : n - i + n;
 			for (int j = 0; j < row; j++) {
-				System.out.print("*");
+				System.out.print("* ");
 			}
 			System.out.println();
 		}
