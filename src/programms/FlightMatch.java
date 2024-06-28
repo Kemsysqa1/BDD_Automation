038ad4b86ebdd1b188a73a9b1b6fd73053fa7b2f
Vinod7875 <vinod.narwade@kemsys.com>
2024-06-28 11:52:40 +0530

    added

---
+++ src/programms/FlightMatch.java
@@ -0,0 +1,17 @@
+package programms;
+
+public class FlightMatch {
+
+	public static void main(String[] args) {
+		int[] source = { 1, 2, 3, 4 };
+		int[] destination = { 1, 2, 3, 4 };
+
+		for (int i = 0; i < source.length; i++) {
+			for (int j = i; j < destination.length; j++) {
+				System.out.println(source[i] + "" + destination[j]);
+
+			}
+		}
+	}
+
+}
